7b5195e1aeb06cee99ef726d89499af4b7ff9911
ErMamone <ermamone@gmail.com>
2021-06-08 16:45:43 -0300

    Se agrega encriptacion de Password, configuracion de conexion y persistencia de usuario

---
+++ bd/persistRegister.go
@@ -0,0 +1,29 @@
+package bd
+
+import (
+	"context"
+	"github.com/ErMamone/tWittor/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+/*InsertNewUser function for insert in DB a new user*/
+func InsertNewUser(u models.Usuario) (string, bool, error){
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+
+	defer cancel()
+
+	db := MongoCn.Database("twittor")
+	col := db.Collection("usuarios")
+
+	u.Password, _= EncryptPassword(u.Password)
+
+	result, err := col.InsertOne(ctx, u)
+	if err != nil {
+		return "", false, err
+	}
+
+	ObjID, _ := result.InsertedID.(primitive.ObjectID)
+
+	return ObjID.String(), true, nil
+}
--- routers/registro.go
+++ routers/registro.go
@@ -2,12 +2,48 @@ package routers
 
 import (
 	"encoding/json"
-	"net/http"
 	"github.com/ErMamone/tWittor/bd"
 	"github.com/ErMamone/tWittor/models"
+	"net/http"
 )
 
 /*SignUp is for a new user register  */
 func SignUp(w http.ResponseWriter, r *http.Request){
 	var t models.Usuario
+	err := json.NewDecoder(r.Body).Decode(&t)
+
+	if err != nil {
+		http.Error(w, "Error en los datos recibidos "+err.Error(), 400)
+		return
+	}
+
+	if len(t.Email) == 0 {
+		http.Error(w, "Entity without Email ", 400)
+		return
+	}
+
+	if len(t.Password) <= 6 {
+		http.Error(w, "Password length less than 6 characters ", 400)
+		return
+	}
+
+	_, encontrado, _ := bd.CheckingIfUserExists(t.Email)
+
+	if encontrado {
+		http.Error(w, "This email is alredy used ", 400)
+		return
+	}
+
+	_, status, err := bd.InsertNewUser(t)
+	if err != nil {
+		http.Error(w, "Error persisting "+err.Error(), 400)
+		return
+	}
+
+	if !status {
+		http.Error(w, "Status problem ", 400)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
